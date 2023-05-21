fce4c838e131b07bf53d3fcad23730a1720eac7f
Marcel-Siudmak <siudmakmarcel7@gmail.com>
2023-05-21 18:48:41 +0200

    poprawiony skrypt dodanie animowanego scrolla i poprawa sekcji home na u mobilnych

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ var initialColor = '#FFFFFF';
 var targetColor = '#121212';
 
 // Utwórz zmienną przechowującą liczbę pikseli, po których ma nastąpić zmiana koloru
-var scrollThreshold = 500;
+var scrollThreshold = window.innerHeight;
 
 // Utwórz funkcję, która zostanie wywołana przy przewijaniu strony
 function changeBackgroundColor() {
@@ -15,12 +15,19 @@ function changeBackgroundColor() {
   // Oblicz procentowy postęp przewijania w zakresie od 0 do 1
   var scrollProgress = currentScrollPosition / scrollThreshold;
 
-  // Jeśli procentowy postęp przewijania jest mniejszy lub równy 1, kontynuuj zmianę koloru
-  if (scrollProgress <= 1) {
-    // Oblicz wartości RGB dla aktualnego koloru na podstawie postępu przewijania
-    var r = Math.round(parseInt(initialColor.slice(1, 3), 16) * (1 - scrollProgress) + parseInt(targetColor.slice(1, 3), 16) * scrollProgress);
-    var g = Math.round(parseInt(initialColor.slice(3, 5), 16) * (1 - scrollProgress) + parseInt(targetColor.slice(3, 5), 16) * scrollProgress);
-    var b = Math.round(parseInt(initialColor.slice(5, 7), 16) * (1 - scrollProgress) + parseInt(targetColor.slice(5, 7), 16) * scrollProgress);
+  // Oblicz przeskalowany postęp przewijania
+  var scaledProgress = Math.pow(scrollProgress, 2);
+
+  // Jeśli currentScrollPosition jest większe niż scrollThreshold, ustaw kolor tła na #121212
+  if (currentScrollPosition > scrollThreshold) {
+    document.body.style.backgroundColor = targetColor;
+  }
+  // W przeciwnym razie kontynuuj zmianę koloru na podstawie przeskalowanego postępu przewijania
+  else {
+    // Oblicz wartości RGB dla aktualnego koloru na podstawie przeskalowanego postępu przewijania
+    var r = Math.round(parseInt(initialColor.slice(1, 3), 16) * (1 - scaledProgress) + parseInt(targetColor.slice(1, 3), 16) * scaledProgress);
+    var g = Math.round(parseInt(initialColor.slice(3, 5), 16) * (1 - scaledProgress) + parseInt(targetColor.slice(3, 5), 16) * scaledProgress);
+    var b = Math.round(parseInt(initialColor.slice(5, 7), 16) * (1 - scaledProgress) + parseInt(targetColor.slice(5, 7), 16) * scaledProgress);
 
     // Zastosuj nowy kolor tła do elementu body
     document.body.style.backgroundColor = '#' + r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0');
